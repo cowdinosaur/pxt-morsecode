6eec848ef6d2d8bc6d9afa316eadfd7b5aaed2c6
cowdinosaur <akmal@tinkertanker.com>
2021-12-09 13:09:43 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -90,7 +90,10 @@ namespace morseCode {
     let Z: MorseSignal[] = _characters.Z;
 
     enum MorseCharacter {
-        A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z
+        //% block="A"
+        A = "A",
+        //% block="A"
+        B = "B",
     }
 
     /**
